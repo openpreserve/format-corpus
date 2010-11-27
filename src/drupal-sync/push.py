141473ae87d222bb3f16c4cad816330c49465e35
Andy Jackson <andy@lovelycode.com>
2010-11-27 22:08:55 +0000

    Basic push working.

--- src/drupal-sync/push.py
+++ src/drupal-sync/push.py
@@ -4,9 +4,12 @@ Created on 27 Nov 2010
 @author: anj
 '''
 
+import hmac
+import os.path, sys, time, mimetypes, xmlrpclib, pprint, base64
+import json, lxml, pprint, fido.prepare
+from lxml import objectify
 
-'''
-'''
+pp = pprint.PrettyPrinter()
 
 #def push_pronom(source='pronom/xml/puid.fmt.10.xml'):
 
@@ -30,6 +33,7 @@ if __name__ == "__main__":
     
     timestamp = str(int(time.time()))
     
+    source='pronom/xml/puid.fmt.10.xml'
     doc = objectify.parse( source )
     ff = doc.getroot().report_format_detail.FileFormat;
     
@@ -37,26 +41,52 @@ if __name__ == "__main__":
       'type': 'format',
       'status': 1,
       'promote': 1,
-#      'nid': 3,
+      'nid': 8,
       'uid': user['uid'],
       'name': user['name'],
       'changed': timestamp,
-      'title': ff.FormatName,
-      'body': ff.FormatDescription,
-      'field_shortname' : [{'value': ff.FormatName},],
-     'field_aliases': [{'value': ff.FormatAliases}],
-     'field_apple_uid': [{'value': ''}],
-     'field_conforms_to': [{'nid': ''}],
-     'field_creator': [{'value': 'Adobe'}],
-     'field_encapsulates': [],
-     'field_extensions': [{'value': '1'}, {'value': '2'}],
-     'field_fullname': [{'value': 'Portable Document Format'}],
-     'field_hasformat': [{'nid': ''}],
-     'field_mimetype': [{'value': '4'}],
-     'field_puid': [{'value': 'fmt/11'}],
-     'field_regex': [{'value': '^/%%PDF/'}],
-     'field_shortname': [{'value': 'PDF'}],
-     'field_type': [{'value': ''}],
-     'field_version': [{'value': ''}],
+      
+      'title': ff.FormatName.text,
+      'body': ff.FormatDescription.text,
+      'field_shortname' : [ {'value': ff.FormatName.text}, ],
+      'field_version': [{'value': ff.FormatVersion.text}],
+      'field_fullname': [{'value': ff.FormatName.text}],
+      'field_aliases': [{'value': ff.FormatAliases.text}],
+      'field_creator': [{'value': 'Adobe'}],
+      'field_puid': [{'value': 'fmt/10'}],
+#      'field_apple_uid': [{'value': ''}],
+     
+# Taxonomy-based ones:
+# Use this to list the extensions taxonomy:
+#    pp.pprint( server.taxonomy.getTree(sessid, "1")[0] )   
+# if the given extension is not present, use this to add it.
+# server.taxonomy.saveTerm(sessid, {"vid":"1","name":"tif"})
+      'field_extensions': [{'value': '12' }],
+#      'field_mimetype': [{'value': '4'}],
+#      'field_type': [{'value': ''}],
+
+
+#      'field_conforms_to': [{'nid': ''}],
+#      'field_encapsulates': [],
+#      'field_hasformat': [{'nid': ''}],
+    
+      'field_regex': [{'value': fido.prepare.convert_to_regex(ff.InternalSignature[0].ByteSequence[0].ByteSequenceValue.text) }],
 
     }
+    
+    pprint.pprint(node);
+    
+    try:
+        n = server.node.save(sessid, node)
+        print n, node
+        nn = server.node.get(sessid,n,{})  # DEBUG - get the final node - not needed now that we know it works
+    
+    except xmlrpclib.Fault, err:
+        print "A fault occurred"
+        print "Fault code: %d" % err.faultCode
+        print "Fault string: %s" % err.faultString
+    
+    else:
+        pp.pprint(n) # DEBUG
+        pp.pprint(nn) # DEBUG - dump the final node - not needed now that we know it works
+        
