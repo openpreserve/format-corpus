5bbe7cfe7ec9745b2897d3550ab9b3ab882cd4d1
Andrew Jackson <Andrew.Jackson@bl.uk>
2012-06-27 17:10:14 +0100

    Modified the generator script to honour the new folder structure.

--- sfw/imagemagick-convert/create-variations.sh
+++ sfw/imagemagick-convert/create-variations.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 # Create an raster version of the text file:
-cat ../lorem-ipsum.txt | convert -size 600x caption:@- lorem-ipsum.im.png
+cat ../../fmt/text/plain/lorem-ipsum.txt | convert -size 600x caption:@- ../../fmt/image/png/lorem-ipsum.im.png
 
-# Also create a lossy version
-convert lorem-ipsum.im.png lorem-ipsum.im.png.im.jpg
+# Also create a lossy version, two ways
+cat ../../fmt/text/plain/lorem-ipsum.txt | convert -size 600x caption:@- ../../fmt/image/jpeg/lorem-ipsum.im.jpg
+convert ../../fmt/image/png/lorem-ipsum.im.png ../../fmt/image/jpeg/lorem-ipsum.im.png.im.jpg
+
+convert -version
 
