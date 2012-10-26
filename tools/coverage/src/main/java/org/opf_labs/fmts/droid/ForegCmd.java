de122bd69e256c8f5b560f4774626f8961a7d658
Andrew Jackson <Andrew.Jackson@bl.uk>
2012-10-27 00:08:52 +0100

    Improved ForegCmd so download is in clearer folders.

--- tools/coverage/src/main/java/org/opf_labs/fmts/droid/ForegCmd.java
+++ tools/coverage/src/main/java/org/opf_labs/fmts/droid/ForegCmd.java
@@ -6,12 +6,17 @@ package org.opf_labs.fmts.droid;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.math.BigInteger;
 import java.net.Authenticator;
 import java.net.PasswordAuthentication;
+import java.net.URL;
 
 import javax.xml.bind.JAXBException;
 
+import org.apache.commons.io.IOUtils;
+
 import eu.planets_project.ifr.core.techreg.formats.SigFileUtils;
+import uk.gov.nationalarchives.pronom.FileFormatType;
 import uk.gov.nationalarchives.pronom.SigFile;
 
 /**
@@ -31,7 +36,7 @@ public class ForegCmd {
 	 */
 	public static void main(String[] args) throws Exception  {
 		//downloadSigFile();
-		SigFileUtils.downloadAllPronomFormatRecords(new File("data/pronom"));
+		downloadAllPronomFormatRecords(new File("data/pronom"));
 		//pythonInvoker();
 	}
 	
@@ -69,5 +74,38 @@ public class ForegCmd {
 	    }
 	}
 
+	public static void downloadAllPronomFormatRecords(File outputFolder) throws Exception {
+		SigFile sigFile = SigFileUtils.getLatestSigFile();
+		BigInteger version = sigFile.getFFSignatureFile().getVersion();
+		System.out.println("Got version "+version);
+		String filename = "droid-signature-file.xml";
+		try {
+			SigFileUtils.writeSigFileTypeToOutputStream( sigFile.getFFSignatureFile(),
+					new FileOutputStream( new File( outputFolder, filename) ) );
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw(e);
+		}
+		//
+		try {
+			for( FileFormatType fft : sigFile.getFFSignatureFile().getFileFormatCollection().getFileFormat() ) {
+				String puid = fft.getPUID();
+				String puidFilename = puid+".xml";
+				FileOutputStream fos = new FileOutputStream(new File(outputFolder, puidFilename) );
+				URL repurl = SigFileUtils.getPronomUrlForPUID(puid);
+				System.out.println("Downloading "+repurl+" to "+outputFolder+"/"+puidFilename );
+				IOUtils.copy( repurl.openStream(), fos );
+				fos.flush();
+				fos.getChannel().force(true);
+				fos.close();
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw(e);
+		}
+
+	}
 
 }
